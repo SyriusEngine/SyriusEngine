ac352663bfba123dfbbb8c4ab8ebbc212ba8ab24
artick788 <60552965+artick788@users.noreply.github.com>
2024-06-08 14:20:57 +0200

    began working on IBL

--- src/SyriusEngine/Renderer/RenderGraph/Passes/SkyBoxPass.cpp
+++ src/SyriusEngine/Renderer/RenderGraph/Passes/SkyBoxPass.cpp
@@ -4,14 +4,40 @@ namespace Syrius{
 
     SkyBoxPass::SkyBoxPass(ResourceView<Context>& context, Resource<ShaderLibrary>& shaderLibrary, const SkyBoxPassDesc& sbDesc):
     RenderPass(context, shaderLibrary, "SkyBoxPass", PassIDGenerator<SkyBoxPass>::getID()) {
+        setupCaptureFrameBuffer();
+        setRadianceMap(sbDesc.radianceMap);
 
     }
 
-    SkyBoxPass::~SkyBoxPass() {
-
-    }
+    SkyBoxPass::~SkyBoxPass() = default;
 
     void SkyBoxPass::execute(PassMap& passMap) {
 
+    }
+
+    void SkyBoxPass::setRadianceMap(const Resource<Image> &radianceMap) {
+
+    }
+
+    void SkyBoxPass::setupCaptureFrameBuffer() {
+        auto layout = m_Context->createFrameBufferLayout();
+        CubeColorAttachmentDesc ccaDesc;
+        ccaDesc.width = SKYBOX_MAP_SIZE;
+        ccaDesc.height = SKYBOX_MAP_SIZE;
+        ccaDesc.format = SR_TEXTURE_RGBA_F16;
+        ccaDesc.clearColor[0] = 0.0f;
+        ccaDesc.clearColor[1] = 0.0f;
+        ccaDesc.clearColor[2] = 0.0f;
+        ccaDesc.clearColor[3] = 1.0f;
+        layout->addCubeColorAttachmentDesc(ccaDesc);
+
+        ViewportDesc vpDesc;
+        vpDesc.width = SKYBOX_MAP_SIZE;
+        vpDesc.height = SKYBOX_MAP_SIZE;
+        layout->addViewportDesc(vpDesc);
+
+        layout->configure();
+
+        m_CaptureFrameBuffer = m_Context->createFrameBuffer(layout);
     }
 }
--- src/SyriusEngine/Renderer/RenderGraph/Passes/SkyBoxPass.hpp
+++ src/SyriusEngine/Renderer/RenderGraph/Passes/SkyBoxPass.hpp
@@ -4,6 +4,8 @@
 
 namespace Syrius{
 
+    constexpr uint32 SKYBOX_MAP_SIZE = 512;
+
     struct SkyBoxPassDesc{
         Resource<Image> radianceMap;
     };
@@ -16,7 +18,16 @@ namespace Syrius{
 
         void execute(PassMap& passMap) override;
 
+        void setRadianceMap(const Resource<Image>& radianceMap);
+
     private:
+
+        void setupCaptureFrameBuffer();
+
+    private:
+
+        ResourceView<FrameBuffer> m_CaptureFrameBuffer;
+        ResourceView<Shader> m_EquirectangularConversionShader;
 
     };
 }
