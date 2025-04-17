79e15ca2615abd5a9d4d9e12363d498310c9ecdd
artick788 <60552965+artick788@users.noreply.github.com>
2025-04-17 18:45:36 +0200

    Added base RenderGraphLayer

---
+++ src/Renderer/RenderGraph/RenderGraphLayer.cpp
@@ -0,0 +1,70 @@
+#include "RenderGraphLayer.hpp"
+
+namespace Syrius {
+
+    void RenderGraphLayer::create(const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::destroy(const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::onRender(const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::onResize(u32 width, u32 height, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::createMesh(MeshID meshID, const Mesh &mesh, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::destroyMesh(MeshID meshID, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::setMeshTransform(MeshID meshID, const Transform &transform, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::createCamera(CameraID cameraID, const Camera &camera, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::updateCamera(CameraID cameraID, const Camera &camera, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::destroyCamera(CameraID cameraID, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::createLight(LightID lightID, const Light &light, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::updateLight(LightID lightID, const Light &light, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::destroyLight(LightID lightID, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::createProjection(ProjectionID projectionID, const Projection &projection, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::updateProjection(ProjectionID projectionID, const Projection &projection, const ResourceView<Context> &ctx) {
+
+    }
+
+    void RenderGraphLayer::destroyProjection(ProjectionID projectionID, const ResourceView<Context> &ctx) {
+
+    }
+
+
+}
---
+++ src/Renderer/RenderGraph/RenderGraphLayer.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <SyriusEngine/Renderer/RenderLayer.hpp>
+
+namespace Syrius {
+
+    class RenderGraphLayer: public IRenderLayer {
+    public:
+        RenderGraphLayer() = default;
+        
+        ~RenderGraphLayer() override = default;
+
+        void create(const ResourceView<Context>& ctx);
+
+        void destroy(const ResourceView<Context>& ctx);
+
+        void onRender(const ResourceView<Context>& ctx) override;
+
+        void onResize(u32 width, u32 height, const ResourceView<Context>& ctx) override;
+
+        void createMesh(MeshID meshID, const Mesh& mesh, const ResourceView<Context>& ctx) override;
+
+        void destroyMesh(MeshID meshID, const ResourceView<Context>& ctx) override;
+
+        void setMeshTransform(MeshID meshID, const Transform& transform, const ResourceView<Context>& ctx) override;
+
+        void createCamera(CameraID cameraID, const Camera& camera, const ResourceView<Context>& ctx) override;
+
+        void updateCamera(CameraID cameraID, const Camera& camera, const ResourceView<Context>& ctx) override;
+
+        void destroyCamera(CameraID cameraID, const ResourceView<Context>& ctx) override;
+
+        void createLight(LightID lightID, const Light& light, const ResourceView<Context>& ctx) override;
+
+        void updateLight(LightID lightID, const Light& light, const ResourceView<Context>& ctx) override;
+
+        void destroyLight(LightID lightID, const ResourceView<Context>& ctx) override;
+
+        void createProjection(ProjectionID projectionID, const Projection& projection, const ResourceView<Context>& ctx) override;
+
+        void updateProjection(ProjectionID projectionID, const Projection& projection, const ResourceView<Context>& ctx) override;
+
+        void destroyProjection(ProjectionID projectionID, const ResourceView<Context>& ctx) override;
+
+
+    };
+}
